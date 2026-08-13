3602cc03ece03a4e054354412988d848b66f38a3
momzain-hub <pitizain@gmail.com>
2026-08-13 16:03:00 +0700

    Load ticker search and mobile radar UX

--- assets/js/signal-monitor-v2-loader.js
+++ assets/js/signal-monitor-v2-loader.js
@@ -1,20 +1,27 @@
 /* Analisaku Technical Monitor cache-busting loader */
 (function(){
-  const VERSION='20260813-1550';
+  const VERSION='20260813-1605';
   const base=document.currentScript?.src||location.href;
 
   function loadStyles(){
-    document.querySelectorAll('link[href*="signal-monitor.css"],link[href*="signal-monitor-plus.css"]').forEach(el=>el.remove());
+    document.querySelectorAll('link[href*="signal-monitor.css"],link[href*="signal-monitor-plus.css"],link[href*="signal-monitor-mobile.css"]').forEach(el=>el.remove());
 
-    const baseCss=document.createElement('link');
-    baseCss.rel='stylesheet';
-    baseCss.href=new URL('../css/signal-monitor.css',base).href+'?v='+VERSION;
-    document.head.appendChild(baseCss);
+    ['../css/signal-monitor.css','../css/signal-monitor-plus.css','../css/signal-monitor-mobile.css'].forEach(path=>{
+      const link=document.createElement('link');
+      link.rel='stylesheet';
+      link.href=new URL(path,base).href+'?v='+VERSION;
+      document.head.appendChild(link);
+    });
+  }
 
-    const plusCss=document.createElement('link');
-    plusCss.rel='stylesheet';
-    plusCss.href=new URL('../css/signal-monitor-plus.css',base).href+'?v='+VERSION;
-    document.head.appendChild(plusCss);
+  function loadEnhancer(){
+    document.querySelectorAll('script[data-analisaku-radar-enhance]').forEach(el=>el.remove());
+    delete document.documentElement.dataset.radarUxReady;
+    const enhance=document.createElement('script');
+    enhance.src=new URL('signal-monitor-enhance.js',base).href+'?v='+VERSION;
+    enhance.async=true;
+    enhance.dataset.analisakuRadarEnhance='true';
+    document.head.appendChild(enhance);
   }
 
   function loadV2(){
@@ -31,6 +38,7 @@
     s.src=new URL('signal-monitor.js',base).href+'?v='+VERSION;
     s.async=true;
     s.dataset.analisakuMonitorV2='true';
+    s.onload=loadEnhancer;
     document.head.appendChild(s);
   }
 
