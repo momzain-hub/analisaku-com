cd5eee44461f0b57dc92de5de14b38f13d58e3d0
momzain-hub <pitizain@gmail.com>
2026-08-16 00:13:45 +0700

    Bump top-level Technical cache for final style entry

--- assets/js/main.js
+++ assets/js/main.js
@@ -276,7 +276,7 @@ updateTechReadiness();
 
 /* ---------- Technical page: TradingView + Signal Monitor V2 ---------- */
 if(document.querySelector('.technical-hero')){
-  const VERSION='20260814-0845';
+  const VERSION='20260816-0025';
 
   const tvModule=document.createElement('script');
   tvModule.src=assetUrl('tradingview.js','assets/js/tradingview.js')+'?v='+VERSION;
