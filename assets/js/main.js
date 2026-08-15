e4ce81d8e92c02ebd61c2f977330efea0a20de0a
momzain-hub <pitizain@gmail.com>
2026-08-16 00:43:27 +0700

    Bump Technical execution plan cache chain

--- assets/js/main.js
+++ assets/js/main.js
@@ -276,7 +276,7 @@ updateTechReadiness();
 
 /* ---------- Technical page: TradingView + Signal Monitor V2 ---------- */
 if(document.querySelector('.technical-hero')){
-  const VERSION='20260816-0025';
+  const VERSION='20260816-0045';
 
   const tvModule=document.createElement('script');
   tvModule.src=assetUrl('tradingview.js','assets/js/tradingview.js')+'?v='+VERSION;
