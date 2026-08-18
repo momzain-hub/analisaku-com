ebe0154dd40944403d158a6b1f26fa429537170e
momzain-hub <pitizain@gmail.com>
2026-08-18 14:16:49 +0700

    Bump Technical cache version for Decision action UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -276,7 +276,7 @@ updateTechReadiness();
 
 /* ---------- Technical page: TradingView + Signal Monitor V2 ---------- */
 if(document.querySelector('.technical-hero')){
-  const VERSION='20260816-0100';
+  const VERSION='20260818-1412';
 
   const tvModule=document.createElement('script');
   tvModule.src=assetUrl('tradingview.js','assets/js/tradingview.js')+'?v='+VERSION;
