bdcfad15d2283d3b5cb2fadf289082ec0c8db87c
momzain-hub <pitizain@gmail.com>
2026-09-15 21:38:56 +0700

    Load yearly Wealth market value breakdown

--- assets/js/wealth.js
+++ assets/js/wealth.js
@@ -1,4 +1,4 @@
-/* Analisaku Wealth v6 bootstrap */
+/* Analisaku Wealth v7 bootstrap */
 (function(){
   const current=document.currentScript;
   const url=relative=>current?new URL(relative,current.src).href:relative;
@@ -33,7 +33,7 @@
   async function init(){
     loadCss(url('../css/wealth-v4.css?v=20260915-1815'),'wealthV4');
     loadCss(url('../css/wealth-products.css?v=20260915-2045'),'wealthProducts');
-    loadCss(url('../css/wealth-future-value.css?v=20260915-2145'),'wealthFutureValue');
+    loadCss(url('../css/wealth-future-value.css?v=20260915-2205'),'wealthFutureValue');
 
     try{
       await loadScript(url('wealth-product-intro.js?v=20260915-2115'));
@@ -49,8 +49,9 @@
 
     try{
       await loadScript(url('wealth-unified.js?v=20260915-2145'));
+      await loadScript(url('wealth-yearly-breakdown.js?v=20260915-2205'));
     }catch(error){
-      console.error('Wealth v6 gagal dimuat',error);
+      console.error('Wealth v7 gagal dimuat',error);
     }
   }
 
