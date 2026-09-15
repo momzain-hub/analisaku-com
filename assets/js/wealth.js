2aa9fb56f7b849e6195ac98b2b071923756b2953
momzain-hub <pitizain@gmail.com>
2026-09-15 18:11:43 +0700

    Load Wealth plain-language method explainer

--- assets/js/wealth.js
+++ assets/js/wealth.js
@@ -32,12 +32,13 @@
 
   async function init(){
     loadCss(url('../css/wealth-v2.css?v=20260915-1430'),'wealthV2');
-    loadCss(url('../css/wealth-v3.css?v=20260915-1805'),'wealthV3');
+    loadCss(url('../css/wealth-v3.css?v=20260915-1810'),'wealthV3');
 
     try{
-      await loadScript(url('wealth-engine.js?v=20260915-1805'));
+      await loadScript(url('wealth-method-ui.js?v=20260915-1810'));
+      await loadScript(url('wealth-engine.js?v=20260915-1810'));
       await loadScript('https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',{crossorigin:'anonymous',referrerpolicy:'no-referrer'});
-      await loadScript(url('wealth-report.js?v=20260915-1805'));
+      await loadScript(url('wealth-report.js?v=20260915-1810'));
     }catch(error){
       console.error('Wealth v3 gagal dimuat',error);
     }
